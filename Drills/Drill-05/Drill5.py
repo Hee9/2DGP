b8f82703cb36a0fdf98a171e5016efce8969dce4
Hee9 <39309373+Hee9@users.noreply.github.com>
2018-09-17 18:32:55 +0900

    Drill 5 Step 16
    2015182010 김희구

--- Drills/Drill-05/Drill5.py
+++ Drills/Drill-05/Drill5.py
@@ -270,15 +270,15 @@ def move_to_point_712_348():
     pass
 
 while True:
-    #move_to_point_203_535()
-    #move_to_point_132_243()
-    #move_to_point_535_470()
-    #move_to_point_477_203()
-    #move_to_point_715_136()
-    #move_to_point_316_225()
-    #move_to_point_510_92()
-    #move_to_point_692_518()
-    #move_to_point_682_336()
+    move_to_point_203_535()
+    move_to_point_132_243()
+    move_to_point_535_470()
+    move_to_point_477_203()
+    move_to_point_715_136()
+    move_to_point_316_225()
+    move_to_point_510_92()
+    move_to_point_692_518()
+    move_to_point_682_336()
     move_to_point_712_348()
 
 
