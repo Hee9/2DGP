76d412c40bbacac18db3077dbe6edb7bdb0d3400
Hee9 <39309373+Hee9@users.noreply.github.com>
2018-09-17 18:08:14 +0900

    Drill 5 Step 9
    2015182010 김희구

--- Drills/Drill-05/Drill5.py
+++ Drills/Drill-05/Drill5.py
@@ -173,6 +173,19 @@ def move_to_point_510_92():
         x += 5
         delay(0.05)
         get_events()
+
+    x = 510
+    y = 225
+    frame = 0
+    while (y > 92):
+        clear_canvas()
+        grass.draw(400, 30)
+        character.clip_draw(frame * 100, 100, 100, 100, x, y)
+        update_canvas()
+        frame = (frame + 1) % 8
+        y -= 5
+        delay(0.05)
+        get_events()
     pass
 def move_to_point_692_518():
     pass
