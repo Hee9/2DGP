56a088ca13f2473af0d778c82ef8fa63fb696719
Hee9 <39309373+Hee9@users.noreply.github.com>
2018-09-17 15:01:42 +0900

    Drill 5 Step 1
    2015182010 김희구

---
+++ Drills/Drill-05/Drill5.py
@@ -0,0 +1,42 @@
+from pico2d import *
+open_canvas()
+grass = load_image('grass.png')
+character = load_image('animation_sheet.png')
+
+def move_to_point_203_535():
+    pass
+def move_to_point_132_243():
+    pass
+def move_to_point_535_470():
+    pass
+def move_to_point_477_203():
+    pass
+def move_to_point_715_136():
+    pass
+def move_to_point_316_225():
+    pass
+def move_to_point_510_92():
+    pass
+def move_to_point_692_518():
+    pass
+def move_to_point_682_336():
+    pass
+def move_to_point_712_348():
+    pass
+
+
+while True:
+    move_to_point_203_535()
+    move_to_point_132_243()
+    move_to_point_535_470()
+    move_to_point_477_203()
+    move_to_point_715_136()
+    move_to_point_316_225()
+    move_to_point_510_92()
+    move_to_point_692_518()
+    move_to_point_682_336()
+    move_to_point_712_348()
+
+
+close_canvas()
+
