60c16fd46e94027bb89fabaa78fb087b868b6dd4
Hee9 <39309373+Hee9@users.noreply.github.com>
2018-10-02 16:58:14 +0900

    Drill #7-1 완성
    2015182010 김희구
    커밋(Step) 총 5회
    https://youtu.be/17wW6Zl4J08

--- Drills/Drill-07-01/Drill #7-1.py
+++ Drills/Drill-07-01/Drill #7-1.py
@@ -3,7 +3,7 @@ import random
 
 KPU_WIDTH, KPU_HEIGHT = 1280, 1024
 
-points = [(random.randint(0,1280),random.randint(0,1024)) for i in range(20)]
+points = [(random.randint(0, 1280), random.randint(0, 1024)) for i in range(20)]
 
 def handle_events():
     global running
@@ -23,12 +23,12 @@ frame = 0
 n = 1
 
 def draw_point(p):
-    global frame,n
+    global frame, n
     clear_canvas()
     kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
 
     if points[n-1][0] > points[n][0]:
-        character.clip_draw(frame*100,0,100,100,p[0],p[1])
+        character.clip_draw(frame*100, 0, 100, 100, p[0], p[1])
     else:
         character.clip_draw(frame * 100, 100, 100, 100, p[0], p[1])
 
@@ -37,25 +37,25 @@ def draw_point(p):
 
 def draw_line(p1,p2):
 
-    for i in range(0,100,2):
-        t = i /100
+    for i in range(0, 100, 2):
+        t = i / 100
         x = (1-t)*p1[0] + t*p2[0]
         y = (1-t)*p1[1] + t*p2[1]
-        draw_point((x,y))
+        draw_point((x, y))
         delay(0.05)
 
     draw_point(p2)
 
-open_canvas(KPU_WIDTH,KPU_HEIGHT)
+open_canvas(KPU_WIDTH, KPU_HEIGHT)
 
 kpu_ground = load_image('KPU_GROUND.png')
 character = load_image('animation_sheet.png')
 
 while True:
 
-    draw_line(points[n-1],points[n])
+    draw_line(points[n-1], points[n])
 
-    n = (n+1)%20
+    n = (n+1) % 20
 
     delay(0.05)
 
