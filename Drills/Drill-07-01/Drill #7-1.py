e35ed27dc1e137b3f684f2f582a4aa5d4553f2b4
Hee9 <39309373+Hee9@users.noreply.github.com>
2018-10-02 16:50:17 +0900

    Drill #7-1 step 4
    2015182010 김희구

--- Drills/Drill-07-01/Drill #7-1.py
+++ Drills/Drill-07-01/Drill #7-1.py
@@ -5,6 +5,19 @@ KPU_WIDTH, KPU_HEIGHT = 1280, 1024
 
 points = [(random.randint(0,1280),random.randint(0,1024)) for i in range(5)]
 
+def handle_events():
+    global running
+    global x,y
+
+    events = get_events()
+
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
+
 x, y = KPU_WIDTH // 2, KPU_HEIGHT // 2
 frame = 0
 n = 1
