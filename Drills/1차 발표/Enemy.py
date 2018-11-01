6ee7c9328ef7fa50b6ee1adb6994e020774594e1
Hee9 <39309373+Hee9@users.noreply.github.com>
2018-11-02 01:53:47 +0900

    텀프 커밋
    커밋

--- Drills/1차 발표/Enemy.py
+++ Drills/1차 발표/Enemy.py
@@ -5,15 +5,15 @@ import game_world
 
 # Enemy Run Speed
 PIXEL_PER_METER = (10.0 / 0.3)  # 10 pixel 30cm
-RUN_SPEED_KMPH = 10.0 # km / hour
+RUN_SPEED_KMPH = 20.0 # km / hour
 RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
 RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
 RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
 
 # Enemy Action Speed
-TIME_PER_ACTION = 0
-ACTION_PER_TIME = 0
-FRAMES_PER_ACTION = 0
+TIME_PER_ACTION = 0.5
+ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
+FRAMES_PER_ACTION = 8
 
 # Enemy States
 
@@ -21,7 +21,7 @@ class RunState:
 
     @staticmethod
     def enter(enemy, event):
-        pass
+        enemy.dir = clamp(-1, enemy.velocity, 1)
 
     @staticmethod
     def exit(enemy, event):
@@ -29,21 +29,21 @@ class RunState:
 
     @staticmethod
     def do(enemy):
-        enemy.frame = (enemy.frame + 1) % 3
+        enemy.frame = (enemy.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 3
         enemy.x += enemy.velocity * game_framework.frame_time
         enemy.x = clamp(25, enemy.x, 1600 - 25)
 
     @staticmethod
     def draw(enemy):
         if enemy.dir == 1:
-            enemy.image.clip_draw(int(enemy.frame) * 100, 100, 100, 100, enemy.x, enemy.y)
+            enemy.image.clip_draw(int(enemy.frame) * 66, 254, 66, 66, enemy.x, enemy.y)
         else:
-            enemy.image.clip_draw(int(enemy.frame) * 100, 0, 100, 100, enemy.x, enemy.y)
+            enemy.image.clip_draw(int(enemy.frame) * 66, 198, 66, 66, enemy.x, enemy.y)
 
 class Enemy:
 
     def __init__(self):
-        self.x, self.y = 1600 // 2, 90
+        self.x, self.y = 780, 988
         # Enemy is only once created, so instance image loading is fine
         self.image = load_image('enemy.png')
         self.dir = 1
